5d3b72eaedd2f6963a1bfe5d17b2d121bb2dba8f
Oleg Pasichnyk <oleg.pasichnyk@kuehne-nagel.com>
2023-02-26 15:27:02 +0200

    Refactoring of TransactionQueryService. Load was split to Load Strategies.

--- transaction/src/main/java/ee/qrental/transaction/application/service/strategy/TransactionLoadStrategyByYearAndWeek.java
+++ transaction/src/main/java/ee/qrental/transaction/application/service/strategy/TransactionLoadStrategyByYearAndWeek.java
@@ -7,15 +7,10 @@ import ee.qrental.transaction.domain.Transaction;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Component;
 
-import java.time.DayOfWeek;
-import java.time.LocalDate;
 import java.util.List;
 
-import static java.time.DayOfWeek.MONDAY;
-import static java.time.DayOfWeek.SUNDAY;
-import static java.time.Month.JUNE;
-import static java.time.temporal.IsoFields.WEEK_OF_WEEK_BASED_YEAR;
-import static java.time.temporal.TemporalAdjusters.previousOrSame;
+import static ee.qrental.transaction.application.service.strategy.TransactionLoadUtils.getFirstDayOfWeekInYear;
+import static ee.qrental.transaction.application.service.strategy.TransactionLoadUtils.getLastDayOfWeekInYear;
 
 @Component
 @AllArgsConstructor
@@ -32,18 +27,9 @@ public class TransactionLoadStrategyByYearAndWeek implements TransactionLoadStra
     public List<Transaction> load(TransactionFilterRequest request) {
         final var year = request.getYear();
         final var weekNumber = request.getWeek().getNumber();
-        final var startDate = getDayByYearAndWeekNumberAndDayOfWeek(year, weekNumber, MONDAY);
-        final var endDate = getDayByYearAndWeekNumberAndDayOfWeek(year, weekNumber, SUNDAY);
 
-        return transactionLoadPort.loadAllBetweenDays(startDate, endDate);
-    }
-
-    private LocalDate getDayByYearAndWeekNumberAndDayOfWeek(
-            final Integer year,
-            final Integer weekNumber,
-            final DayOfWeek dayOfWeek) {
-        return LocalDate.of(year, JUNE, 1)
-                .with(previousOrSame(dayOfWeek))
-                .with(WEEK_OF_WEEK_BASED_YEAR, weekNumber);
+        return transactionLoadPort.loadAllBetweenDays(
+                getFirstDayOfWeekInYear(year, weekNumber),
+                getLastDayOfWeekInYear(year, weekNumber));
     }
 }
